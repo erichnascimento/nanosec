10780600c1939d59bbad98f1130f49fdfd5828ce
Erich Nascimento <erichnascimento@gmail.com>
2018-06-05 02:35:32 -0300

    Expose redis methods

--- storage/key_value_storage.go
+++ storage/key_value_storage.go
@@ -5,7 +5,10 @@ import "github.com/alicebob/miniredis"
 type KeyValueStorage interface {
 	SetAdd(k string, elems ...string) (int, error)
 	SRem(k string, fields ...string) (int, error)
+	Members(k string) ([]string, error)
 	IsMember(k, v string) (bool, error)
+	Set(k, v string) error
+	Get(k string) (string, error)
 	Close()
 }
 
